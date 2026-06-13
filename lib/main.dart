0d5a8d4f6dba1e9b463f3f8073a9e4bde5e231c1
ThetPaingWin <thetpaingwin.dev@gmail.com>
2026-06-13 16:59:43 +0630

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:sirius_face_detection_kit/camera/camera_preview.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    final interpreter = await Interpreter.fromAsset(
+      'assets/tf_models/mobilefacenet.tflite',
+    );
+
+    print('SUCCESS');
+  } catch (e, s) {
+    print('FAILED: $e');
+
+    print(s);
+  }
   runApp(MyApp());
 }
 
